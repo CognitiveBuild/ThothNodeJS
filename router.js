c7247faefda7a8215663ff79c5924a79e55a92f4
liugreatsea <liugreatsea@163.com>
2016-11-02 11:04:38 +0800

    bug fix

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
 
-var mc = require("./control/messageControl");
-// var moc = require("./control/messageOperateControl");
+'use strict';
+
+var mc = require("./control/messagecontrol");
 var greeting = require("./control/greetingControl");
-var gr = require("./control/graphicControl");
+var gr = require("./control/graphiccontrol");
 
 var router = function(app, logmessages) {
     app.post('/api/greeting', greeting.greeting);
     app.post('/api/message', mc.message);
-    // app.post('/api/messageOperate', moc.messageOperate);
     app.get('/api/graphic', gr.graphic);
 };
 
